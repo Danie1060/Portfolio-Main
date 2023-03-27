a35a04d21403411bf81d4d05f1c566a7831d6459
Daniel <newsomed14@gmail.com>
2023-03-27 10:31:05 -0400

    Add random message to script and link to HTML

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+const randomNum = (num) => Math.floor(Math.random()* num);
+
+const good = {
+    affirmation: ["beautiful", "smart", "worthy", "empowered", "unique", "called"],
+    gratitude: ["family", "friends", "work", "food", "love"],
+    verse:["Colossians 2:10", "1 Thessalonians 1:4", "1 John 3:1", "Matthew 5:14", "Romans 8:37", "Galatians 4:7"],
+};
+
+let positive = () => {
+    console.log(`I am ${good.affirmation[randomNum(good.affirmation.length)]}.`);
+    console.log(`I am thankful for ${good.gratitude[randomNum(good.gratitude.length)]}.`);
+    console.log(`Today's Verse: ${good.verse[randomNum(good.verse.length)]}`);
+}
+positive();
